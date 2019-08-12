57f35adee2f01b071cc3701d9eab186ccd93b412
XuChao <xuchao_1213@163.com>
2019-08-12 16:56:56 +0800

    :sparkles:support db redis config while bootstrap

--- bootx.go
+++ bootx.go
@@ -13,13 +13,47 @@ func Bootstrap(app Application, configs ...interface{}) {
 	logger.Printf("%s %s bootstrap ...", appName, appVersion)
 	initKernel()
 	defer cleanupKernel()
-	//todo init with configs
-
-	webInit()
+	webConf, dbConf, redisConf := initModules(configs...)
+	//if no web config ,use default config
+	if !webConf {
+		webInit()
+	}
+	if dbConf {
+		defer dbCleanup()
+	}
+	if redisConf {
+		defer redisCleanup()
+	}
 	app.Bootstrap()
 	defer app.Shutdown()
-	//启动Web服务
+	//start web service
 	Web().start()
 	defer Web().stop()
 	getKernel().waitForExit()
+}
+
+func initModules(configs ...interface{}) (webConf, dbConf, redisConf bool) {
+	for _, conf := range configs {
+		switch c := conf.(type) {
+		case WebConfig:
+			webInitWithConfig(&c)
+		case *WebConfig:
+			webInitWithConfig(c)
+			webConf = true
+			break
+		case DBConfig:
+			dbInitWithConfig(&c)
+		case *DBConfig:
+			dbInitWithConfig(c)
+			dbConf = true
+			break
+		case RedisConfig:
+			redisInitWithConfig(&c)
+		case *RedisConfig:
+			redisInitWithConfig(c)
+			redisConf = true
+			break
+		}
+	}
+	return
 }
